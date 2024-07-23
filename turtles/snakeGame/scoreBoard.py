4ce25cff8c9967f7bea6c5483b97a6d5911e6193
Drawich <gustafsson.angelica@gmail.com>
2024-07-23 17:40:19 -0500

    Update scoreBoard.py
    Implement game reset functionality and display "GAME OVER!" message
    
    - Updated ScoreBoard class to correctly display "GAME OVER!" message and reset the game score upon collision with walls or snake tail.
    - Implemented a high score feature to save the highest score in new txt file and display at top of screen.

--- turtles/snakeGame/scoreBoard.py
+++ turtles/snakeGame/scoreBoard.py
@@ -1,8 +1,10 @@
 from turtle import Turtle
+import os
 
 ALIGNMENT = "center"
 FONT = ("Courier", 20, "normal")
 COLOR = "white"
+SCOREBOARD_POSITION = (0, 280)
 
 
 class ScoreBoard(Turtle):
@@ -10,20 +12,22 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = self.load_highscore()
         self.setup_scoreboard()
 
     def setup_scoreboard(self):
         """Set up the initial scoreboard stating score = 0"""
         self.penup()
         self.hideturtle()
-        self.goto(0,280)
+        self.color(COLOR)
+        self.goto(SCOREBOARD_POSITION)
         self.update_scoreboard()
 
     def update_scoreboard(self):
         """Updates the scoreboard with the current score"""
         self.clear()
-        self.color(COLOR)
-        self.write(f"SCORE: {self.score}", align=ALIGNMENT, font=FONT)
+        self.goto(SCOREBOARD_POSITION)
+        self.write(f"SCORE: {self.score} HIGH SCORE: {self.highscore}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         """Increase and displays score with +1 when snake collides with food"""
@@ -36,3 +40,27 @@ class ScoreBoard(Turtle):
         self.color(COLOR)
         self.write(f"GAME OVER!", align=ALIGNMENT, font=FONT)
 
+    def reset_game(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            self.save_highscore()
+        self.score = 0
+        self.update_scoreboard()
+
+    def load_highscore(self):
+        """Load highscore from a file if it exists"""
+        highscore_file = "highscore.txt"
+        if os.path.exists(highscore_file):
+            with open(highscore_file, "r") as file:
+                try:
+                    return int(file.read().strip())
+                except ValueError:
+                    return 0
+        else:
+            return 0
+
+    def save_highscore(self):
+        """Save highscore to a file"""
+        highscore_file = "highscore.txt"
+        with open(highscore_file, "w") as file:
+            file.write(str(self.highscore))
